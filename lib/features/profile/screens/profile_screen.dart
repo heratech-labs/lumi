131ed7befd1f3f939f6790969a33f4c9e81ecbf6
sergiocaminitti <sergiocaminitti@gmail.com>
2025-11-01 11:59:46 -0300

    added bottom nav bar in screens

--- lib/features/profile/screens/profile_screen.dart
+++ lib/features/profile/screens/profile_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import '../../../app/config/app_routes.dart';
+import '../../../common/widgets/layout/lumi_bottom_nav_bar.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
@@ -8,6 +10,9 @@ class ProfileScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: const Text('Perfil')),
       body: const Center(child: Text('Tela de Perfil - Em desenvolvimento')),
+      bottomNavigationBar: const LumiBottomNavBar(
+        currentRoute: AppRoutes.profile,
+      ),
     );
   }
 }
